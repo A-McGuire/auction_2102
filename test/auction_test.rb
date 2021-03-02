771901b73eeebe37d9ff33d493ea1411b01b6e70
Aidan McGuire <>
2021-03-02 13:29:04 -0700

    Complete i1

--- test/auction_test.rb
+++ test/auction_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/attendee'
 require './lib/auction'
+require './lib/item'
 
 class AuctionTest < MiniTest::Test
 
@@ -9,21 +10,27 @@ class AuctionTest < MiniTest::Test
     auction = Auction.new
     
     assert_instance_of Auction, auction
-    
     assert_equal [], auction.items
   end
   
   def test_add_item
-    auction = Auction.new
     item1 = Item.new('Chalkware Piggy Bank')
     item2 = Item.new('Bamboo Picture Frame')
-    # attendee = Attendee.new(name: 'Megan', budget: '$50')
+    auction = Auction.new
     
     auction.add_item(item1)
     auction.add_item(item2)
     
-    auction.items
-    assert_equal [item1, item2]
+    assert_equal [item1, item2], auction.items
+  end
+  
+  def test_list_item
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    auction = Auction.new
+    
+    auction.add_item(item1)
+    auction.add_item(item2)
     
     assert_equal ["Chalkware Piggy Bank", "Bamboo Picture Frame"], auction.item_names
   end
